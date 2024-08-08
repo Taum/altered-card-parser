35ce97d2b01f8917ad7311bba7beb173a65ed062
Taum <taum.xd@gmail.com>
2024-08-08 01:10:52 -0400

    WIP: parsing 35pct of database

---
+++ scripts/parse-all.ts
@@ -0,0 +1,45 @@
+import * as fs from 'fs';
+import {
+    CollectionEntry,
+} from '../src/models/api';
+
+import { IToken, Grammars } from 'ebnf';
+
+const grammar = fs.readFileSync('src/grammar.ebnf', { encoding: "utf8" })
+
+// Debug at https://menduz.github.io/ebnf-highlighter/
+let parser = new Grammars.W3C.Parser(grammar, { debug: false });
+
+const collectionTxt = fs.readFileSync("data/uniques-extd.json", { encoding: "utf8" })
+const uniques = JSON.parse(collectionTxt) as Array<CollectionEntry>
+
+let stats = { parsed: 0, failed: 0, partial: 0 }
+let fails: Array<String> = []
+
+console.log("Parsing ", uniques.length, " entries")
+
+for (let uniq of uniques) {
+    if (uniq.name == "Foiler") { continue; }
+
+    let cardEls = uniq.elements
+    if (cardEls.MAIN_EFFECT) {
+        let mainAST = parser.getAST(cardEls.MAIN_EFFECT);
+        if (mainAST) {
+            if (mainAST.rest) {
+                stats.partial += 1
+                fails.push("[" + uniq.name + "]" + " ..." + cardEls.MAIN_EFFECT)
+            } else {
+                stats.failed += 1
+                stats.parsed += 1
+            }
+        } else {
+            stats.failed += 1
+            fails.push("[" + uniq.name + "]" + cardEls.MAIN_EFFECT)
+        }
+    }
+}
+
+console.log("Done!\n", stats)
+console.log("Fully parsed: ", Math.round(1000 * stats.parsed / (stats.parsed + stats.failed)) / 10.0, "%")
+
+fs.writeFileSync("temp/rejects.txt", fails.join("\n"))
